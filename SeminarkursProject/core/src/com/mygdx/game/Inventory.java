6214c6ba2101fdad7dfd7dcf7628d8f4953d3447
Implodee <33040161+Implodee@users.noreply.github.com>
2018-06-17 16:07:32 +0200

    Crafting Part 1

--- SeminarkursProject/core/src/com/mygdx/game/Inventory.java
+++ SeminarkursProject/core/src/com/mygdx/game/Inventory.java
@@ -1,6 +1,7 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.utils.Array;
+import com.mygdx.game.Item.ItemId;
 import com.mygdx.game.Item.ItemList;
 import com.mygdx.game.Item.ItemMaster;
 
@@ -19,11 +20,12 @@ public class Inventory {
     }
     public void addStarterItems(){
         addItem(ItemList.axe_t1(),1);
+        addItem(ItemList.coal(20),20);
+        addItem(ItemList.mat_wood(20),20);
     }
     public boolean addItem(ItemMaster item, int quantity){
         for(int i = 0; i < inventorySize; i++) {
-            if (!slots.get(i).isEmpty() && slots.get(i).isItemOfType(item) == true)
-            {
+            if (!slots.get(i).isEmpty() && slots.get(i).isItemOfType(item) == true){
                 if (i == inventorySize){
                     for(int o = 0; o < inventorySize; o++) {
                         if (slots.get(o).isEmpty() == true) {
@@ -37,8 +39,18 @@ public class Inventory {
                         }
                     }
                 } else {
-                    slots.get(i).addItem(item, slots.get(i).getQuantity()+quantity);
-                    return true;
+                    if(slots.get(i).getQuantity()+quantity <= slots.get(i).getItem().getStackSizeMax()){
+                        slots.get(i).addItem(item, slots.get(i).getQuantity()+quantity);
+                        return true;
+                    } else if(slots.get(i).getQuantity()+quantity > slots.get(i).getItem().getStackSizeMax()){
+                        if(i < inventorySize){
+                            i++;
+                            slots.get(i).addItem(item,quantity);
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    }
                 }
             } else if(slots.get(i).isEmpty()) {
                 slots.get(i).addItem(item, quantity);
@@ -47,18 +59,22 @@ public class Inventory {
         }
         return false;
     }
-    public boolean removeItem(ItemMaster item, int quantity){
+
+    public int getSize(){
+        return inventorySize;
+    }
+
+    public boolean removeItem(ItemId id, int quantity){
         for(int i = 0; i < inventorySize; i++){
-            if(slots.get(i).isItemOfType(item)==true && slots.get(i).getQuantity() - quantity >= 0){
+            if(slots.get(i).getID() == id && slots.get(i).getQuantity() - quantity >= 0){
                 slots.get(i).removeItem(quantity);
+                System.out.println("Items successfully removed");
                 return true;
             } else {
-                if(slots.get(i).isItemOfType(item)==true&&slots.get(i).getQuantity() - quantity < 0){
+                if(slots.get(i).getID() == id && slots.get(i).getQuantity() - quantity <= 0){
                     System.out.println("Quantity of item not big enough to remove");
-                    return false;
                 } else {
                     System.out.println("No items to remove found");
-                    return false;
                 }
             }
         }
@@ -70,6 +86,14 @@ public class Inventory {
     }
     public Slot getSlot(int i){
         return slots.get(i);
+    }
+    public boolean hasItem(ItemId id, int quantity){
+        for(int i = 0; i < inventorySize; i++){
+            if(getSlot(i).getID() == id && getSlot(i).getQuantity() >= quantity){
+                return true;
+            }
+        }
+        return false;
     }
     public class Slot {
         private ItemMaster item;
@@ -102,6 +126,11 @@ public class Inventory {
             } else {
                 return false;
             }
+        }
+        public ItemId getID(){
+            if(item != null){
+                return this.item.getId();
+            } else {return null;}
         }
     }
 }
--- SeminarkursProject/core/src/com/mygdx/game/InventoryGUI.java
+++ SeminarkursProject/core/src/com/mygdx/game/InventoryGUI.java
@@ -1,21 +1,21 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.NinePatch;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Matrix4;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.NinePatchDrawable;
 import com.badlogic.gdx.utils.Disposable;
 import com.mygdx.game.Actor.Actor;
 import com.mygdx.game.Item.ItemId;
+import com.mygdx.game.Item.ItemList;
 
 /**
  * Created by Implodee on 20.11.2017.
@@ -23,20 +23,92 @@ import com.mygdx.game.Item.ItemId;
 
 public class InventoryGUI extends Actor implements Disposable{
 
-    private final SpriteBatch sb = new SpriteBatch();
     private final OrthographicCamera camera;
     private final NinePatchDrawable background = new NinePatchDrawable(new NinePatch(new Texture("red.png")));
     private final Stage stage = new Stage();
-    private Skin skin = new Skin();
-    private Label inventoryLabel;
+    Label.LabelStyle ls = new Label.LabelStyle(new BitmapFont(), Color.BLACK);
     private Table table;
+    private Table fullInv;
+    private Table craftingTable;
+    private final int SELECTION = 0;
+    private final int CRAFTING = 1;
+    private final int INVENTORY = 2;
+    private int mode;
+    private int craftingMode;
 
+    // LIST ALL CRAFTING ITEMS (LABEL) AND STRINGS AND CONSTANTS FOR MODE, RECIPES
+    private final int furnaceCostWood = 10;
+    private final int furnaceCostCoal = 10;
+    private final int intFurnace = 0;
+    private final String stringFurnace = new String("Furance");
+    private Label craftingFurnace = new Label(stringFurnace, ls);
+
+    private final int intPowerLine = 1;
+    private final String stringPowerLine = new String("Powerline");
+    private Label craftingPowerline = new Label(stringPowerLine, ls);
+
+    //INT FOR MAX AVAILABLE CRAFTING RECIPES (-1), ALWAYS UPDATE MANUALLY
+    private int craftingMax = 1;
 
     public InventoryGUI(OrthographicCamera camera){
         this.camera = camera;
-        Label.LabelStyle ls = new Label.LabelStyle(new BitmapFont(), Color.BLACK);
-
-        inventoryLabel = new Label("Inventory", ls); table = new Table();
+        setupFullInventoryTable();
+        setupCraftingTable();
+        setupHotbar();
+        mode = 0;
+        stage.addActor(table);
+    }
+    private void setupCraftingTable(){
+        craftingTable = new Table();fullInv.setHeight(0.1f);
+        craftingTable.bottom();
+        craftingTable.setFillParent(false);
+        craftingTable.debugAll();
+        craftingTable.pad(0.5f);
+        craftingTable.defaults().expandX();
+        craftingTable.setBounds(0,0, Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+        craftingTable.row();
+        craftingTable.add(new Label("crafting", ls)).colspan(4);
+        craftingTable.row();
+        craftingTable.add(new Label("item to craft", ls));
+        craftingTable.add(new Label("cost", ls)).colspan(3);
+        craftingTable.row();
+        craftingMode = 0;
+        craftingPowerline.setText(stringPowerLine);
+        craftingFurnace.setText("-> craft Furnace");
+        //RECIPES FOR CRAFTING, UPDATE MANUALLY.
+        craftingTable.add(craftingFurnace);
+        craftingTable.add(new Label(furnaceCostCoal + " coal", ls));
+        craftingTable.add(new Label(furnaceCostWood + " wood", ls));
+        craftingTable.add(new Label("", ls));
+        craftingTable.row();
+        craftingTable.add(craftingPowerline);
+        craftingTable.add(new Label("", ls));
+        craftingTable.add(new Label("", ls));
+        craftingTable.add(new Label("", ls));
+    }
+    private void setupFullInventoryTable(){
+        fullInv = new Table();
+        fullInv.setHeight(0.1f);
+        fullInv.bottom();
+        fullInv.setFillParent(false);
+        fullInv.debugAll();
+        fullInv.pad(0.5f);
+        fullInv.defaults().expandX();
+        fullInv.setBounds(0,0, Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+        for(int i = 0; i < Inventory.playerInventory.getSize(); i++){
+            if(Inventory.playerInventory.getSlot(i).isEmpty()){
+                fullInv.add(new Label("No Item", ls));
+                fullInv.add(new Label(0 + "", ls));
+                fullInv.row();
+            } else if(Inventory.playerInventory.getSlot(i).isEmpty()==false) {
+                fullInv.add(new Label(Inventory.playerInventory.getSlot(i).getItem().getDesc(),ls));
+                fullInv.add(new Label(Inventory.playerInventory.getSlot(i).getQuantity()+"",ls));
+                fullInv.row();
+            }
+        }
+    }
+    private void setupHotbar(){
+        table = new Table();
         table.setHeight(0.1f);
         table.bottom();
         table.setFillParent(false);
@@ -45,7 +117,6 @@ public class InventoryGUI extends Actor implements Disposable{
         table.pad(0.5f);
         table.defaults().expandX();
         table.setBounds(0,0, Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
-        table.add(inventoryLabel).colspan(2);
         table.row();
         for(int i = 0; i < 3; i++){
             if(Inventory.playerInventory.getSlot(i).isEmpty()){
@@ -58,29 +129,83 @@ public class InventoryGUI extends Actor implements Disposable{
                 table.row();
             }
         }
-        stage.addActor(table);
     }
-
     public void render(){
         Matrix4 hudMatrix = camera.combined.cpy();
         hudMatrix.setToOrtho2D(0,0,5,5);
         hudMatrix.scale(1,1,1);
-
         stage.draw();
+        if(Gdx.input.isKeyJustPressed(Input.Keys.RIGHT)){
+            if(mode < 2){
+                mode++;
+                if(mode == CRAFTING){
+                    table.remove();
+                    setupCraftingTable();
+                    stage.addActor(craftingTable);
+                    craftingMode = 0;
+                }
+                if(mode == INVENTORY){
+                    craftingTable.remove();
+                    setupFullInventoryTable();
+                    stage.addActor(fullInv);
+                }
+            }
+        }
+        if(Gdx.input.isKeyJustPressed(Input.Keys.LEFT)){
+            if(mode > 0){
+                mode--;
+                if(mode == CRAFTING){
+                    fullInv.remove();
+                    setupCraftingTable();
+                    stage.addActor(craftingTable);
+                    craftingMode = 0;
+                }
+                if(mode == SELECTION){
+                    craftingTable.remove();
+                    setupHotbar();
+                    stage.addActor(table);
+                }
+            }
+        }
+        if(mode == CRAFTING){
+            if(Gdx.input.isKeyJustPressed(Input.Keys.DOWN) && craftingMode < craftingMax){
+                craftingMode++;
+                if(craftingMode == intPowerLine){
+                    craftingFurnace.setText(stringFurnace);
+                    craftingPowerline.setText("-> craft Powerline");
+                }
+            }
+            if(Gdx.input.isKeyJustPressed(Input.Keys.UP) && craftingMode > 0){
+                craftingMode--;
+                if(craftingMode == intFurnace){
+                    craftingPowerline.setText(stringPowerLine);
+                    craftingFurnace.setText("-> craft Furnace");
+                }
+            }
+            if(Gdx.input.isKeyJustPressed(Input.Keys.ENTER)){
+                switch(craftingMode){
+                    //Craft Furnace; Recipe HERE
+                    case intFurnace:
+                        if(Inventory.playerInventory.hasItem(ItemId.COAL, furnaceCostCoal) &&
+                           Inventory.playerInventory.hasItem(ItemId.MAT_WOOD, furnaceCostWood)){
+                            //remove items
+                            Inventory.playerInventory.removeItem(ItemId.COAL, 10);
+                            Inventory.playerInventory.removeItem(ItemId.MAT_WOOD,10);
+                            //add items
+                            Inventory.playerInventory.addItem(ItemList.furnace(), 1);
+                        }
+                        break;
+                }
+            }
+        }
     }
-
     @Override
     public void dispose() {
-        //sb.dispose();
         stage.dispose();
     }
-
     @Override
     public void update(float dt){
-        //if item added / removed from inventory, update;
-        //code to come
     }
-
     public ItemId getId() {
         System.out.println("InvewntoryGUI needs to be implemented");
         return null;
